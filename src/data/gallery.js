4c750e7691d8d9ee91d3f11d15573662b403865e
jasmith188 <jasmith.188@gmail.com>
2026-05-28 11:27:18 -0500

    update some photos

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,17 +1,18 @@
 // import asianChopstick from "../assets/asian-chopstick.
+import steak from '../assets/filet-asparagus.png';
 import burrata from '../assets/burrata.jpg';
 import chickenMarsala from '../assets/chicken-marsala.jpg';
-import lobsterRavioli from '../assets/lobster-ravioli.jpeg';
+import lobsterRavioli from '../assets/lobster-ravioli.png';
 import mushroomPasta from '../assets/mushroom-pasta.png';
 import scallops from '../assets/scallops-side.jpg';
-import steak from '../assets/steak.png';
 
 export const gallery = [
+  { src: steak, title: 'Filet Mignon with Asparagus' },
   { src: burrata, title: 'Burrata, Basil Oil & Crostini' },
   { src: chickenMarsala, title: 'Chicken Marsala' },
   { src: lobsterRavioli, title: 'Lobster Ravioli' },
   { src: mushroomPasta, title: 'Mushroom Pasta' },
   { src: scallops, title: 'Seared Scallops' },
-  { src: steak, title: 'Duck and Cherry Demi-Glace' },
+
   // { src: asianChopstick, title: "Asian Inspired Rice Bowl" },
 ];
