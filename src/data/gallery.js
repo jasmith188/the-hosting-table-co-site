1e74f30fa9c3258842f8509fec25a385b85ab264
jasmith188 <jasmith.188@gmail.com>
2026-06-06 10:25:33 -0500

    add more images to gallery

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,13 +1,18 @@
 // import asianChopstick from "../assets/asian-chopstick.
 import steak from '../assets/filet-asparagus.png';
 import chickenMarsala from '../assets/chicken-marsala.jpg';
+import filetAuPoivre from '../assets/filet-au-poivre.jpeg';
 import lobsterRavioli from '../assets/lobster-ravioli.png';
 import mushroomPasta from '../assets/mushroom-pasta.png';
 import scallops from '../assets/scallops-side.jpg';
+import moroccanShortRib from '../assets/moroccan-short-rib.jpeg';
 import beefAndBroccoli from '../assets/beef-and-broccoli.jpg';
 import brusselsCeasarSalad from '../assets/brussels-ceasar-salad.jpeg';
+import roastedCarrots from '../assets/roasted-carrots.jpeg';
+import shrimpAndGrits from '../assets/shrimp-grits.jpeg';
 import burrata from '../assets/burrata.jpg';
 import eggsBenedict from '../assets/egss-benedict.jpeg';
+import skirtSteak from '../assets/skirt-steak-chimichurri.png';
 import friedChickenBenedict from '../assets/fried-chicken-biscuit-benedict.jpeg';
 import orangeChicken from '../assets/orange-chicken.jpg';
 import pastaVodka from '../assets/pasta-vodka.jpeg';
@@ -15,8 +20,10 @@ import salmonSpaetzel from '../assets/salmon-spaetzel.jpeg';
 
 export const gallery = [
   { src: steak, title: 'Filet Mignon with Asparagus' },
+  { src: roastedCarrots, title: 'Roasted Carrots with Tahini' },
   { src: chickenMarsala, title: 'Chicken Marsala' },
   { src: lobsterRavioli, title: 'Lobster Ravioli' },
+  { src: filetAuPoivre, title: 'Filet Au Poivre' },
   { src: mushroomPasta, title: 'Mushroom Pasta' },
   { src: scallops, title: 'Seared Scallops' },
   { src: beefAndBroccoli, title: 'Beef and Broccoli' },
@@ -27,6 +34,9 @@ export const gallery = [
   { src: orangeChicken, title: 'Orange Chicken' },
   { src: pastaVodka, title: 'Pasta Vodka' },
   { src: salmonSpaetzel, title: 'Salmon with Spaetzel' },
+  { src: skirtSteak, title: 'Skirt Steak with Chimichurri' },
+  { src: moroccanShortRib, title: 'Moroccan Short Rib' },
+  { src: shrimpAndGrits, title: 'Shrimp and Grits' },
 
   // { src: asianChopstick, title: "Asian Inspired Rice Bowl" },
 ];
