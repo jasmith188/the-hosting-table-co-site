e7739de57397d6c5adf90eac58b279181c096046
jasmith188 <jasmith.188@gmail.com>
2026-06-15 10:17:54 -0500

    allow only usa on site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer>
       <p className="footer-brand">The Hosting Table Co.</p>
       <p>
-        Chef Jimmy Smith |{" "}
+        Chef Jimmy |{" "}
         <a href="mailto:jimmy@thehostingtableco.com">jimmy@thehostingtableco.com</a>
       </p>
       <p>Serving Madison, Middleton, Sun Prairie, Baraboo, Wisconsin Dells, Lake Delton & surrounding areas.</p>
