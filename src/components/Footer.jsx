bb590d3d97bc8e49df8913bf9e98d27599c59660
jasmith188 <jasmith.188@gmail.com>
2026-06-22 15:31:03 -0500

    make changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,6 @@ export default function Footer() {
         <a href="tel:+16084770652">608-477-0652</a> |{" "}
         <a href="mailto:jimmy@thehostingtableco.com">jimmy@thehostingtableco.com</a>
       </p>
-      <p>Serving Baraboo, Wisconsin Dells, Lake Delton, Madison, Middleton, Waunakee, Sun Prairie & surrounding South Central Wisconsin areas.</p>
     </footer>
   );
 }
