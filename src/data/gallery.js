424eeb3f3601d1e04ac3d19da0193925ed950c2f
jasmith188 <jasmith.188@gmail.com>
2026-07-18 13:28:06 -0500

    update gLLERY FOOD PICS

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,8 +1,8 @@
 // import asianChopstick from "../assets/asian-chopstick.
-import steak from "../assets/filet-asparagus.png";
+// import steak from "../assets/filet-asparagus.png";
 import chickenMarsala from "../assets/chicken-marsala.jpg";
 import filetAuPoivre from "../assets/filet-au-poivre.jpeg";
-import lobsterRavioli from "../assets/lobster-ravioli.png";
+// import lobsterRavioli from "../assets/lobster-ravioli.png";
 import mushroomPasta from "../assets/mushroom-pasta.png";
 import scallops from "../assets/scallops-side.jpg";
 import moroccanShortRib from "../assets/moroccan-short-rib.jpeg";
@@ -12,7 +12,7 @@ import roastedCarrots from "../assets/roasted-carrots.jpeg";
 import shrimpAndGrits from "../assets/shrimp-grits.jpeg";
 import burrata from "../assets/burrata.jpg";
 import eggsBenedict from "../assets/egss-benedict.jpeg";
-import skirtSteak from "../assets/skirt-steak-chimichurri.PNG";
+// import skirtSteak from "../assets/skirt-steak-chimichurri.PNG";
 import friedChickenBenedict from "../assets/fried-chicken-biscuit-benedict.jpeg";
 import orangeChicken from "../assets/orange-chicken.jpg";
 import pastaVodka from "../assets/pasta-vodka.jpeg";
@@ -22,9 +22,9 @@ export const gallery = [
   { src: filetAuPoivre, title: "Filet Au Poivre" },
   { src: roastedCarrots, title: "Roasted Carrots with Tahini" },
   { src: chickenMarsala, title: "Chicken Marsala" },
-  { src: lobsterRavioli, title: "Lobster Ravioli" },
+  // { src: lobsterRavioli, title: "Lobster Ravioli" },
   { src: mushroomPasta, title: "Mushroom Pasta" },
-  { src: steak, title: "Filet Mignon with Asparagus" },
+  // { src: steak, title: "Filet Mignon with Asparagus" },
   { src: scallops, title: "Seared Scallops" },
   { src: beefAndBroccoli, title: "Beef and Broccoli" },
   { src: brusselsCeasarSalad, title: "Brussels Caesar Salad" },
@@ -34,7 +34,7 @@ export const gallery = [
   { src: orangeChicken, title: "Orange Chicken" },
   { src: pastaVodka, title: "Pasta Vodka" },
   { src: salmonSpaetzel, title: "Salmon with Spaetzel" },
-  { src: skirtSteak, title: "Skirt Steak with Chimichurri" },
+  // { src: skirtSteak, title: "Skirt Steak with Chimichurri" },
   { src: moroccanShortRib, title: "Moroccan Short Rib" },
   { src: shrimpAndGrits, title: "Shrimp and Grits" },
 
