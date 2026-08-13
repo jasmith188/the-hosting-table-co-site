986bd64ab7df40c0f96cc1d0072865898039ac64
jasmith188 <jasmith.188@gmail.com>
2026-08-13 10:48:21 -0500

    add photos to the gallery

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,18 +1,23 @@
-// import asianChopstick from "../assets/asian-chopstick.
-// import steak from "../assets/filet-asparagus.png";
 import chickenMarsala from "../assets/chicken-marsala.jpg";
 import filetAuPoivre from "../assets/filet-au-poivre.jpeg";
-// import lobsterRavioli from "../assets/lobster-ravioli.png";
+import lobterBisque from "../assets/lobster-bisque.jpeg";
+import honeyCheesecake from "../assets/honey-cheesecake.jpeg";
+import ceviche from "../assets/ceviche.png";
+import latinAmeericanApps from "../assets/latinAmericanApps.jpeg";
+import brunch from "../assets/brunch.jpeg";
 import mushroomPasta from "../assets/mushroom-pasta.png";
 import scallops from "../assets/scallops-side.jpg";
-import moroccanShortRib from "../assets/moroccan-short-rib.jpeg";
+import salmonCharcutterie from "../assets/salmonCharcuterie.jpeg";
 import beefAndBroccoli from "../assets/beef-and-broccoli.jpg";
+import crabCake from "../assets/crab-cake.jpeg";
 import brusselsCeasarSalad from "../assets/brussels-ceasar-salad.jpeg";
 import roastedCarrots from "../assets/roasted-carrots.jpeg";
+import summerBBQApps from "../assets/summerBBQApps.jpeg";
+import kingCrab from "../assets/king-crab.jpeg";
 import shrimpAndGrits from "../assets/shrimp-grits.jpeg";
 import burrata from "../assets/burrata.jpg";
+import lemonTart from "../assets/lemon-tart.jpeg";
 import eggsBenedict from "../assets/egss-benedict.jpeg";
-// import skirtSteak from "../assets/skirt-steak-chimichurri.PNG";
 import friedChickenBenedict from "../assets/fried-chicken-biscuit-benedict.jpeg";
 import orangeChicken from "../assets/orange-chicken.jpg";
 import pastaVodka from "../assets/pasta-vodka.jpeg";
@@ -20,13 +25,20 @@ import salmonSpaetzel from "../assets/salmon-spaetzel.jpeg";
 
 export const gallery = [
   { src: filetAuPoivre, title: "Filet Au Poivre" },
+  { src: lobterBisque, title: "Lobster Bisque" },
   { src: roastedCarrots, title: "Roasted Carrots with Tahini" },
+  { src: crabCake, title: "Crab Cake" },
   { src: chickenMarsala, title: "Chicken Marsala" },
-  // { src: lobsterRavioli, title: "Lobster Ravioli" },
+  { src: ceviche, title: "Ceviche" },
+  { src: kingCrab, title: "King Crab" },
+  { src: honeyCheesecake, title: "Honey Cheesecake" },
+  { src: brunch, title: "Coffee Rubbed Beef Tenderloin, Crab Cake Benedicts" },
+  { src: latinAmeericanApps, title: "Latin American Appetizers" },
   { src: mushroomPasta, title: "Mushroom Pasta" },
-  // { src: steak, title: "Filet Mignon with Asparagus" },
   { src: scallops, title: "Seared Scallops" },
   { src: beefAndBroccoli, title: "Beef and Broccoli" },
+  { src: salmonCharcutterie, title: "Salmon Charcuterie" },
+  { src: summerBBQApps, title: "Summer BBQ Appetizers" },
   { src: brusselsCeasarSalad, title: "Brussels Caesar Salad" },
   { src: burrata, title: "Burrata, Basil Oil & Crostini" },
   { src: eggsBenedict, title: "Eggs Benedict" },
@@ -34,9 +46,5 @@ export const gallery = [
   { src: orangeChicken, title: "Orange Chicken" },
   { src: pastaVodka, title: "Pasta Vodka" },
   { src: salmonSpaetzel, title: "Salmon with Spaetzel" },
-  // { src: skirtSteak, title: "Skirt Steak with Chimichurri" },
-  { src: moroccanShortRib, title: "Moroccan Short Rib" },
   { src: shrimpAndGrits, title: "Shrimp and Grits" },
-
-  // { src: asianChopstick, title: "Asian Inspired Rice Bowl" },
 ];
